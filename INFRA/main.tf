d49b31c5fa9975b5f2d7b6ade2273bf3b86ed21b
costaoliveiraricrdo <costa.oliveira.ricardo@gmail.com>
2022-07-05 17:34:15 -0300

    Criando repositorios para arquivos INFRA DEV e PRD

---
+++ INFRA/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.regiao_aws
+}
+
+resource "aws_instance" "Web_Server" {
+  ami           = "ami-08d4ac5b634553e16"
+  instance_type = var.instancia
+  key_name      = var.chave
+  vpc_security_group_ids = [
+    "sg-05ab2fb5bca4c0c1d",
+    "sg-01c755d675702f501",
+    "sg-0677f61f65d157377",
+    "sg-08812bf19eddffd8c"
+  ]
+  tags = {
+    Name = "Web_Server_Ubu_02"
+  }
+}
+
+resource "aws_key_pair" "chaveSSH" {
+  key_name = var.chave
+  public_key = file("${var.chave}.pub")
+
+}

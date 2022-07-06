8d5bfefb00db73c7d30a6496c2636c3e191cd6f6
costaoliveiraricrdo <costa.oliveira.ricardo@gmail.com>
2022-07-06 14:55:02 -0300

    Update main.tf PRD file

--- PRD/main.tf
+++ PRD/main.tf
@@ -1,7 +1,7 @@
 module "aws-prd" {
     source = "../INFRA"
     instancia = "t2.small"
-    regiao_aws = "us-east-1"
+    regiao_aws = "us-east-2"
     chave = "IaC-PRD"
     nome_host = "PRD_Web_Server_01"
 }

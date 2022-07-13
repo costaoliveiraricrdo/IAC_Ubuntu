9d0cf1041a9679ef497766e7246fbbf4420d5b70
costaoliveiraricrdo <costa.oliveira.ricardo@gmail.com>
2022-07-13 16:36:03 -0300

    Restore last version

--- INFRA/variables.tf
+++ INFRA/variables.tf
@@ -1,45 +1,15 @@
 variable "regiao_aws" {
-  type = string
-  description = "aws region"
+    type = string
 }
 
 variable "chave" {
-  type = string
-  description = "SSH Key"
+    type = string
 }
 
 variable "instancia" {
-  type = string
-  description = "aws type instance"
+    type = string
 }
 
 variable "nome_instancia" {
-  type = string
-  description = "hostname"
-}
-
-variable "infra_env" {
-  type = string
-  description = "infrastructure environment"
-}
-
-variable "vpc_cidr" {
-  type        = string
-  description = "The IP range to use for the VPC"
-  default     = "10.0.0.0/16"
-}
-
-variable "azs" {
-  type = list(string)
-  description = "AZs to create subnets into"
-}
-
-variable "public_subnets" {
-  type = list(string)
-  description = "subnets to create for public network traffic, one per AZ"
-}
-
-variable "private_subnets" {
-  type = list(string)
-  description = "subnets to create for private network traffic, one per AZ"
+    type = string
 }
--- PRD/main.tf
+++ PRD/main.tf
@@ -1,14 +1,8 @@
 module "aws-prd" {
   source         = "../INFRA"
-  instancia      = "t2.micro"
+  instancia      = "t2.small"
   regiao_aws     = "us-east-1"
   chave          = "IaC-PRD"
   nome_instancia = "PRD_Web_Server_01"
-  infra_env      = "Production-Environment"
-  subnets        = module.vpc.vpc_public_subnets
-  security_groups = [module.vpc.security_group_public]
-  tags = {
-    Name = "aws-cloud-${var.infra_env}-web"
-  }
-  create_eip = true
 }
+

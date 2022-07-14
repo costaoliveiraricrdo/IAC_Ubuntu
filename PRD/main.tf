c6529d0515e28a31091ce4296b24ff543eb7507e
costaoliveiraricrdo <costa.oliveira.ricardo@gmail.com>
2022-07-13 21:49:22 -0300

    playbook.yml update

--- PRD/main.tf
+++ PRD/main.tf
@@ -1,6 +1,6 @@
 module "aws-prd" {
   source         = "../INFRA"
-  instancia      = "t2.small"
+  instancia      = "t2.micro"
   regiao_aws     = "us-east-1"
   chave          = "IaC-PRD"
   nome_instancia = "PRD_Web_Server_01"

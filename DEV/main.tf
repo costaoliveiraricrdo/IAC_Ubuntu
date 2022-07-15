d49acba32e6a37f96c0eb549ac0c47ed3bb14022
costaoliveiraricrdo <costa.oliveira.ricardo@gmail.com>
2022-07-15 15:56:25 -0300

    Insert grupo_seguranca variable & output in main.tf DEV

--- DEV/main.tf
+++ DEV/main.tf
@@ -1,8 +1,17 @@
 module "aws-dev" {
-  source         = "../INFRA"
-  instancia      = "t2.micro"
-  regiao_aws     = "us-east-1"
-  chave          = "IaC-DEV"
-  nome_instancia = "DEV_Web_Server_01"
+  source          = "../INFRA"
+  instancia       = "t2.micro"
+  regiao_aws      = "us-east-1"
+  chave           = "IaC-DEV"
+  nome_instancia  = "DEV_Web_Server_01"
+  infra_env       = "development-environment"
+  grupo_seguranca = module.aws-dev.security_group_public
 }
 
+output "IP" {
+  value = module.aws-dev.IP_publico
+}
+
+output "SG_public" {
+  value = module.aws-dev.security_group_public
+}

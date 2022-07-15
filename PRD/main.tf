7ed321b7c9f73370ec6afae01cf3c80532da18cb
costaoliveiraricrdo <costa.oliveira.ricardo@gmail.com>
2022-07-15 15:58:21 -0300

    Insert grupo_seguranca variable & output in main.tf PRD

--- PRD/main.tf
+++ PRD/main.tf
@@ -1,8 +1,17 @@
 module "aws-prd" {
-  source         = "../INFRA"
-  instancia      = "t2.micro"
-  regiao_aws     = "us-east-1"
-  chave          = "IaC-PRD"
-  nome_instancia = "PRD_Web_Server_01"
+  source          = "../INFRA"
+  instancia       = "t2.micro"
+  regiao_aws      = "us-east-1"
+  chave           = "IaC-PRD"
+  nome_instancia  = "PRD_Web_Server_01"
+  infra_env       = "production-environment"
+  grupo_seguranca = module.aws-prd.security_group_public
 }
 
+output "IP" {
+  value = module.aws-prd.IP_publico
+}
+
+output "SG_public" {
+  value = module.aws-prd.security_group_public
+}
